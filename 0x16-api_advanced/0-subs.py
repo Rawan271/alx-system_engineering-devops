ada231f460edd08fa391dd456cbc9f8d9c90f54d
Rawan271 <https://github.com/Rawan271/alx-system_engineering-devops.git>
2024-08-13 19:45:57 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""
-    Uses Reddit API to print the number of subscribers of a subreddit
-"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Get the number of subscribers for a given subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'my-app/0.0.1'}
+
+    # Make a request to the Reddit API for the given subreddit
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    # Check if the request was successful and the subreddit is valid
+    if response.status_code == 200:
+        # Parse the JSON response and return the number of subscribers
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    else:
+        # If the subreddit is invalid or not found, return 0
         return 0
-
-    data = response.json().get("data")
-    num_subs = data.get("subscribers")
-
-    return num_subs
